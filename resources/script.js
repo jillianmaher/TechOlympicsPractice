46a653f413558465ab41c4599cfa05e86757575d
Maher <jillian.maher2@boone.kyschools.us>
2020-02-22 13:01:21 -0500

    Added function to change button colour

--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,17 @@
+
+//VARIABLES
 var heading;
 heading = document.getElementById('colourValue');
+var buttons = document.getElementsByClassName('colourButton');
 
+//MAIN
 heading.innerHTML = 'Hello world!';
+setButtonColour(buttons[0], 0, 0, 255);
+
+
+//FUNCTIONS
+function setButtonColour(button, red, green, blue){
+    button.setAttribute('style',
+                        'background-color: rgb('+ red +','+ green +','+ blue +');'
+                       );
+}
